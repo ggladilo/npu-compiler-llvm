4948e6cff376edb6d061d73fdaeb77b98f3af398
Gladilov, Gleb <gleb.gladilov@intel.com>
2026-02-17 17:38:49 +0000

    Revert "extend SymbolContainer semantics to SymbolUserMap"
    This reverts commit cdbc3be1c4844c57ea14669cd6fef895b7648d40.

--- mlir/lib/IR/SymbolTable.cpp
+++ mlir/lib/IR/SymbolTable.cpp
@@ -119,8 +119,7 @@ walkSymbolTable(Operation *op,
 /// Build a symbol table with the symbols within the given operation.
 SymbolTable::SymbolTable(Operation *symbolTableOp)
     : symbolTableOp(symbolTableOp) {
-  assert((symbolTableOp->hasTrait<OpTrait::SymbolTable>() ||
-          symbolTableOp->hasTrait<OpTrait::SymbolContainer>()) &&
+  assert(symbolTableOp->hasTrait<OpTrait::SymbolTable>() &&
          "expected operation to have SymbolTable trait");
   assert(symbolTableOp->getNumRegions() == 1 &&
          "expected operation to have a single region");
@@ -1112,7 +1111,7 @@ SymbolUserMap::SymbolUserMap(SymbolTableCollection &symbolTable,
 }
 
 void SymbolUserMap::replaceAllUsesWith(Operation *symbol,
-                                       SymbolRefAttr newSymbolName) {
+                                       StringAttr newSymbolName) {
   auto it = symbolToUsers.find(symbol);
   if (it == symbolToUsers.end())
     return;
@@ -1137,11 +1136,6 @@ void SymbolUserMap::replaceAllUsesWith(Operation *symbol,
       newIt.first->second.set_union(oldIt->second);
     symbolToUsers.erase(oldIt);
   }
-}
-
-void SymbolUserMap::replaceAllUsesWith(Operation *symbol,
-                                       StringAttr newSymbolName) {
-  replaceAllUsesWith(symbol, mlir::FlatSymbolRefAttr::get(newSymbolName));
 }
 
 //===----------------------------------------------------------------------===//
